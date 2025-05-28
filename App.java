7c9912ae42deeb7e0e72f8b88ec9c8e919fe48da
bhavishyaverma450 <bhavishyaver50@gmail.com>
2025-05-28 21:08:28 +0530

    making the basic frame for game

--- App.java
+++ App.java
@@ -1,19 +1,21 @@
 import javax.swing.*;
 
-
-
 public class App {
     public static void main(String[] args) {
         int boardWidth = 360;
         int boardHeight = 640;
 
-
         JFrame frame = new JFrame("Flappy Bird");
-        frame.setVisible(true);
-        frame.setLocationRelativeTo(null);
-        frame.setSize(boardWidth,boardHeight);
+        // frame.setLocationRelativeTo(null);
+        frame.setSize(boardWidth, boardHeight);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    }    
+        Flappy_Bird fp = new Flappy_Bird();
+        frame.add(fp);
+        frame.pack();// if this is not used then it will count the size of the bar where flappy bird
+                     // is written in the height and width
+        frame.setVisible(true);
+
+    }
 }
--- Flappy_Bird.java
+++ Flappy_Bird.java
@@ -1,12 +1,59 @@
 import javax.swing.*;
+import java.awt.*;
 
-class Flappy_Bird {
+public class Flappy_Bird extends JPanel {
 
-    Flappy_Bird() {
-        JFrame frame = new JFrame();
+    int boardWidth = 360;
+    int boardHeight = 640;
+
+    // Images
+    Image bgImg;
+    Image birdImg;
+    Image topPipe;
+    Image bottomPipe;
+
+    Bird bird;
+
+    class Bird {
+        int x, y, width, height;
+        Image img;
+
+        public Bird(Image img, int x, int y, int width, int height) {
+            this.img = img;
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+        }
     }
 
-    public static void main(String[] args) {
+    // Constructor
+    public Flappy_Bird() {
+        setPreferredSize(new Dimension(boardWidth, boardHeight)); // Required for pack()
 
+        // Images
+        birdImg = new ImageIcon("flappybird.png").getImage();
+        bgImg = new ImageIcon("flappybirdbg.png").getImage();
+        topPipe = new ImageIcon("toppipe.png").getImage();
+        bottomPipe = new ImageIcon("bottompipe.png").getImage();
+
+        // Bird
+        int birdX = boardWidth / 8;
+        int birdY = boardHeight / 2;
+        int birdW = 34;
+        int birdH = 24;
+        bird = new Bird(birdImg, birdX, birdY, birdW, birdH);
     }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        g.drawImage(bgImg, 0, 0, boardWidth, boardHeight, null);
+        g.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height, null);
+    }
+
 }
