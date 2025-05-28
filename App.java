f47c2d877302560ef8c39e7ca5344cf3557ecc94
bhavishyaverma450 <bhavishyaver50@gmail.com>
2025-05-28 19:41:34 +0530

    start the coding

---
+++ App.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+
+
+public class App {
+    public static void main(String[] args) {
+        int boardWidth = 360;
+        int boardHeight = 640;
+
+
+        JFrame frame = new JFrame("Flappy Bird");
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setSize(boardWidth,boardHeight);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }    
+}
